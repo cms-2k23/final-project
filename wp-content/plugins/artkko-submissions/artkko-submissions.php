0f121fc0f6216279bf71f816bc1768ea5e2948e0
mmich-pl <32057510+mmich-pl@users.noreply.github.com>
2023-12-07 16:47:44 +0100

    feat(plugin): add main plugin class

--- wp-content/plugins/artkko-submissions/artkko-submissions.php
+++ wp-content/plugins/artkko-submissions/artkko-submissions.php
@@ -1,1 +1,26 @@
 <?php
+
+/**
+ *
+ * Plugin Name: Artkko Submissions
+ * Description: This plugin allows users to make submissions fot commissions from their favourite artist.
+ * Version: 0.0.1
+ * Text Domain: options-plugin
+ *
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    die("You should not be here!");
+}
+
+
+if(!class_exists('ArtkkoSubmissions')) {
+
+    class ArtkkoSubmissions
+    {
+
+
+    }
+
+    new ArtkkoSubmissions;
+}
