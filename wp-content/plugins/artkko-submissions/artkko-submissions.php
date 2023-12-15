dc1309107e119dc110405e1603203ea160d8d002
mmich-pl <32057510+mmich-pl@users.noreply.github.com>
2023-12-15 12:58:39 +0100

    feat(db): add plugin table initialization

--- wp-content/plugins/artkko-submissions/artkko-submissions.php
+++ wp-content/plugins/artkko-submissions/artkko-submissions.php
@@ -13,6 +13,36 @@ if (!defined('ABSPATH')) {
     die("You should not be here!");
 }
 
+register_activation_hook( __FILE__, "activate_artkko" );
+function activate_artkko() {
+
+        // WP Globals
+        global $table_prefix, $wpdb;
+
+        // Customer Table
+        $customerTable = $table_prefix . 'artkko_submissions';
+
+        // Create Customer Table if not exist
+        if( $wpdb->get_var( "show tables like '$customerTable'" ) != $customerTable ) {
+
+            // Query - Create Table
+            $sql = "CREATE TABLE `$customerTable` (";
+            $sql .= " `id` int(11) NOT NULL auto_increment, ";
+            $sql .= " `artist_id` int NOT NULL, ";
+            $sql .= " `customer_email` varchar(80) NOT NULL, ";
+            $sql .= " `customer_name` varchar(100), ";
+            $sql .= " `done` boolean NOT NULL, ";
+            $sql .= " `due` date NOT NULL, ";
+            $sql .= " PRIMARY KEY `customer_id` (`id`) ";
+            $sql .= ") ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;";
+
+            // Include Upgrade Script
+            require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
+
+            // Create Table
+            dbDelta( $sql );
+        }
+}
 
 if (!class_exists('ArtkkoSubmissions')) {
 
